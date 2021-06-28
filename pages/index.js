eb97d2d799a522ab6d9caa38da43a67a72260592
Erizta Alifa P <eriztaalifad@gmail.com>
2021-06-28 19:20:54 +0700

    Implementing NextAuth.js
    - install next-auth

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import { getSession } from 'next-auth/client'
 import Head from 'next/head'
 import Header from '../components/Header'
+import Login from '../components/Login'
 
-export default function Home() {
+export default function Home({ session }) {
+  if (!session) return <Login />
   return (
     <div className="">
       <Head>
@@ -9,11 +12,23 @@ export default function Home() {
       </Head>
 
       <Header>
-        
+
       </Header>
+      
       <main>
 
       </main>
     </div>
   )
+}
+
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context)
+
+  return {
+    props: {
+      session
+    }
+  }
 }
